7f0e28a39398ea988fb5ab382bd0b32aacef57f3
Jordan <43543240+HotGreenPeas@users.noreply.github.com>
2021-04-30 20:17:52 -0400

    things needed for firebase implementation
    watch out! I included the API key!

---
+++ views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+import pyrebase
+
+config = {
+    "apiKey": "AIzaSyBNFiTW8_2AeQNQSxlEqBu1qrRKC5S-PKU",
+    "authDomain": "dict-131c6.firebaseapp.com",
+    "databaseURL": "https://dict-131c6-default-rtdb.firebaseio.com/",
+    "projectId": "dict-131c6",
+    "storageBucket": "dict-131c6.appspot.com",
+    "messagingSenderId": "263354360121",
+    "appId": "1:263354360121:web:f71760956a3f3c8f2888e8"
+}
+firebase = pyrebase.initialize_app(config)
+authe = firebase.auth()
+database = firebase.database()
+
+
+def home(request):
+    name = database.child('Data').child('Name').get().val()
+    id = database.child('Data').child('ID').get().val()
+    project = database.child('Data').child('Project').get().val()
+    return render(request, "index.html",
+                  {"name": name, "id": id, "project": project})
