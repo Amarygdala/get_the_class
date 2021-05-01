34ff9069d3c6146acd529244c9aed01a733f463e
Xiang Weng <xiang.weng3@gmail.com>
2021-04-30 22:49:45 -0400

    react setup

--- DiC/settings.py
+++ DiC/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'vtranscribe',
     'front_end',
     'rest_framework'
+    
 
 ]
 
--- DiC/urls.py
+++ DiC/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from vtranscribe import views
+from django.urls import path, include
+from vtranscribe import views as vtranscribeV
+from front_end import views as front_endV
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home,name='home')
+    path('', front_endV.index)
 ]
