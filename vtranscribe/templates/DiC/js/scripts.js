0c4303d7696c6ac4a833827da5d9e1a814eb44a3
xweng1016 <54854234+xweng1016@users.noreply.github.com>
2021-05-01 15:39:08 -0400

    HTML

---
+++ vtranscribe/templates/DiC/js/scripts.js
@@ -0,0 +1,42 @@
+/*!
+* Start Bootstrap - Agency v6.0.5 (https://startbootstrap.com/theme/agency)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
+*/
+(function ($) {
+    "use strict"; // Start of use strict
+
+    // Smooth scrolling using anime.js
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').on('click', function () {
+        if (
+            location.pathname.replace(/^\//, "") ==
+            this.pathname.replace(/^\//, "") &&
+            location.hostname == this.hostname
+        ) {
+            var target = $(this.hash);
+            target = target.length ?
+                target :
+                $("[name=" + this.hash.slice(1) + "]");
+            if (target.length) {
+                anime({
+                    targets: 'html, body',
+                    scrollTop: target.offset().top - 72,
+                    duration: 1000,
+                    easing: 'easeInOutExpo'
+                });
+                return false;
+            }
+        }
+    });
+
+    // Closes responsive menu when a scroll trigger link is clicked
+    $(".js-scroll-trigger").on('click', function () {
+        $(".navbar-collapse").collapse("hide");
+    });
+
+})(jQuery); // End of use strict
+
+$(document).ready(function(){
+    $('#title').focus();
+      $('#text').autosize();
+  });
