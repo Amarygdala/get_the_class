f8974a4991c4ae94b910793b681478c79b8bbbf7
Jordan <43543240+HotGreenPeas@users.noreply.github.com>
2021-05-01 22:57:27 -0400

    add media root

--- DiC/urls.py
+++ DiC/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from vtranscribe import views
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home),
+    path('', views.MainView.as_view(), name='main-view'),
+    path('upload/', views.file_upload_view, name='upload-view'),
     path('transcribe/', views.transcribe),
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
